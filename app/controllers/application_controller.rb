000db08f987e18d3c99639ff3d3291e5c85b4094
absyah <ardian.bahtiarsyah@gmail.com>
2025-03-15 11:18:29 +0700

    add jwt authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    begin
+      decoded = JWT.decode(token, Rails.application.credentials.secrets.secret_key_base)[0]
+      @current_user = User.find(decoded['user_id'])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_15_010143) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
