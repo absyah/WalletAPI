df3a01f84dd2354861c5189e986cf88976c42ad6
absyah <ardian.bahtiarsyah@gmail.com>
2025-03-15 08:03:12 +0700

    add User, Team, and Stock

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_14_173817) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_15_010143) do
+  create_table "stocks", force: :cascade do |t|
+    t.string "symbol"
+    t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "transactions", force: :cascade do |t|
     t.integer "wallet_id", null: false
     t.decimal "amount"
@@ -20,6 +33,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_14_173817) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["wallet_id"], name: "index_transactions_on_wallet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "wallets", force: :cascade do |t|
